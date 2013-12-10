d08d3bdf0443878fcf23e9124ab34e24816a3645
= <=>
2013-12-10 15:48:36 +0400

    Redesign

--- run.py
+++ run.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python
+u'''
+Automation module
+Run actions by simple command
+Usage: run <command>
+'''
 
 from sys import argv
 import sh
 
-def main():
-    if len(argv) != 2:
-        print 'Usage: run <command>'
-        return
-    command = argv[1]
-    if command == 'miredo':
-        sh.miredo()
-    elif command == 'revert':
-        sh.hg.revert("-C", "--all")
-    else:
-        print 'What is "%s"?' % command
+def miredo():
+    u'''
+    Miredo client
+    '''
+    result = sh.miredo()
+    print(result.ran)
+    print(result)
+
+def revert():
+    u'''
+    Hg revert all changes without backup
+    '''
+    result = sh.hg.revert(C=True, all=True)
+    print(result.ran)
+    print(result)
+
+def unknown_command():
+    u'''
+    Command with this name not found
+    '''
+    print('What is "%s"?' % argv[1])
 
 if __name__ == '__main__':
-    main()
+    if len(argv) != 2:
+        print 'Usage: run <command>'
+    else:
+        globals().get(argv[1], unknown_command)()
