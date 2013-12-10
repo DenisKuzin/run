5162220aa74ab8cd3810d3537d64f2d41e9d3c3a
Denis Kuzin <digiholiq@gmail.com>
2013-12-10 17:16:24 +0300

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,9 @@ Usage: run <command>
 '''
 
 from sys import argv
+from os import path
 import sh
+import settings
 
 def miredo():
     u'''
@@ -23,6 +25,47 @@ def revert():
     result = sh.hg.revert(C=True, all=True)
     print(result.ran)
     print(result)
+
+def django():
+    u'''
+    Django development server
+    '''
+    manage_py = sh.python.bake('-t', path.join(settings.trbd_home, 'manage.py'))
+    for message in manage_py('runserver', '0.0.0.0:8080', _iter=True):
+        print(message)
+
+def test():
+    u'''
+    Unit tests on trbd project
+    '''
+    manage_py = sh.python.bake('-t', path.join(settings.trbd_home, 'manage.py'))
+    for message in manage_py('test', '-v', '3', 'documents.FunctionalTests', _iter=True):
+        print(message)
+
+def celery():
+    u'''
+    Celery message queue
+    '''
+    result = sh.rm(path.join(settings.trbd_home, 'celery.log'), f=True)
+    print(result.ran)
+    print(result)
+    manage_py = sh.python.bake('-t', path.join(settings.trbd_home, 'manage.py'))
+    for message in manage_py('celeryd', verbosity=3, settings='settings', concurrency=1, pool='solo', discard=True, logfile=path.join(settings.trbd_home, 'celery.log'), loglevel='DEBUG', traceback=True, hostname='test_worker', _iter=True):
+        print(message)
+
+def office():
+    u'''
+    OpenOffice server
+    '''
+    for message in sh.Command('/opt/openoffice.org3/program/soffice "-accept=socket,host=localhost,port=8100;urp;StarOffice.ServiceManager" -nologo -headless -nofirststartwizard')(_iter=True):
+        print(message)
+
+def rabbit():
+    u'''
+    RabbitMQ Broker
+    '''
+    for message in sh.rabbitmq-server(_iter=True):
+        print(message)
 
 def unknown_command():
     u'''
